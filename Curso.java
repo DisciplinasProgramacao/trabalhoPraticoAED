c323bfed7194845372d6fdf07fe1b711740f39f3
osvaldoferreiraf <115900138+osvaldoferreiraf@users.noreply.github.com>
2023-06-03 17:02:09 -0300

    Update Curso.java

--- Curso.java
+++ Curso.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Curso {
 
     private int cod;
@@ -5,5 +7,62 @@ public class Curso {
     private int quantVagas;
     private Candidato[] listaSelecionados;
     // private Queue<Candidato> filaEspera
+
+    public Curso(){
+
+    }
+
+    public Curso(int cod, String nome, int quantVagas){
+
+    }
+
+    public void inserirFilaEspera(Candidato cand){
+
+    }
+
+    public boolean inserirListaSelecionados(Candidato cand){
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso [cod=" + cod + ", nome=" + nome + ", quantVagas=" + quantVagas + ", listaSelecionados="
+                + Arrays.toString(listaSelecionados) + "]";
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantVagas() {
+        return quantVagas;
+    }
+
+    public void setQuantVagas(int quantVagas) {
+        this.quantVagas = quantVagas;
+    }
+
+    public Candidato[] getListaSelecionados() {
+        return listaSelecionados;
+    }
+
+    public void setListaSelecionados(Candidato[] listaSelecionados) {
+        this.listaSelecionados = listaSelecionados;
+    }
+
+    
+
 
 }
