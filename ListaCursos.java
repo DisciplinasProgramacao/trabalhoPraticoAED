7a45ce07be149c5582157996cf7dbfdba079ecf1
osvaldoferreiraf <115900138+osvaldoferreiraf@users.noreply.github.com>
2023-06-09 08:55:44 -0300

    ListaCursos

--- ListaCursos.java
+++ ListaCursos.java
@@ -6,14 +6,16 @@ public class ListaCursos {
     public ListaCursos() {
     }
 
-    public void inserirFim(Curso x) {
+    public void inserirFim(Curso c) {
+        ultimo.prox = new CelulaCurso(c);
+        ultimo = ultimo.prox;
+
     }
 
     public String mostrar() {
     }
 
     public Curso pesquisar(int codCurso) {
-
 
     }
 
