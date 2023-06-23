70ac69041c1fb9b28b8f07625cb4eabf2e2dd749
Wilian Souza <wilian.santos@sga.pucminas.br>
2023-06-23 09:58:07 -0300

    Update Teste.java

--- Teste.java
+++ Teste.java
@@ -1,6 +1,7 @@
 public class Teste {
     
     public static void main(String[] args) throws Exception{
-        
+        System.out.println("\033[H\033[2J");
+        System.out.println("Teste");
     }
 }
