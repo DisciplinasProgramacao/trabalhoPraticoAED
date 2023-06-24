56e963eb4be43af339f9a97e8c064745ccebad0f
Wilian Souza <wilian.santos@sga.pucminas.br>
2023-06-24 15:21:25 -0300

    Revert "Update Teste.java"
    This reverts commit 70ac69041c1fb9b28b8f07625cb4eabf2e2dd749.

--- Teste.java
+++ Teste.java
@@ -1,7 +1,6 @@
 public class Teste {
     
     public static void main(String[] args) throws Exception{
-        System.out.println("\033[H\033[2J");
-        System.out.println("Teste");
+        
     }
 }
